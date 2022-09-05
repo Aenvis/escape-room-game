87af6aea859081e6167f4d3a2ff61a037b1a5e29
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-09-05 23:13:49 +0200

    Camera rotation and player movement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -8,16 +8,17 @@ namespace Controls
 {
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rb;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-    
+
     private PlayerInputActions m_playerInputActions;
     private Vector2 m_input;
+    private Rigidbody rb;
 
     private void Awake()
     {
         m_playerInputActions = new PlayerInputActions();
+        Cursor.visible = false;
     }
 
     private void OnEnable()
@@ -25,25 +26,33 @@ public class PlayerMovement : MonoBehaviour
         m_playerInputActions.PlayerMovement.Enable();
         m_playerInputActions.PlayerMovement.Jump.performed += JumpPerformed;
     }
-
-
+    
     private void OnDisable()
     {
         m_playerInputActions.PlayerMovement.Disable();
     }
 
+    private void Start()
+    {
+        rb = gameObject.GetComponentInChildren<Rigidbody>();
+    }
+
     private void Update()
     {
         m_input = m_playerInputActions.PlayerMovement.Movement.ReadValue<Vector2>();
+        var dir = new Vector3(m_input.x, 0f, m_input.y).normalized;
+        
+        
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(m_input.x * moveSpeed * Time.fixedDeltaTime, rb.velocity.y, m_input.y * moveSpeed * Time.fixedDeltaTime);
     }
-    private void JumpPerformed(InputAction.CallbackContext conext)
+    
+    private void JumpPerformed(InputAction.CallbackContext context)
     {
-        rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Force);
+        rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
     }
 }
 }
