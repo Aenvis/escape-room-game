764e5fd75bcfdf4ab5fea96425f56068e9d1af43
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-09-19 19:16:39 +0200

    Player walk action

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] float maxMoveSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private Camera playerCam;
+    [SerializeField] private Transform groundCheck;
+    
 
+    private float m_currentMoveSpeed;
     private PlayerInputActions m_playerInputActions;
     private Vector2 m_input;
     private Rigidbody m_rb;
@@ -24,8 +28,11 @@ public class PlayerMovement : MonoBehaviour
     {
         m_playerInputActions.PlayerMovement.Enable();
         m_playerInputActions.PlayerMovement.Jump.performed += JumpPerformed;
+        m_playerInputActions.PlayerMovement.Walk.performed += Walk;
+        m_playerInputActions.PlayerMovement.Walk.canceled += Walk;
     }
-    
+
+
     private void OnDisable()
     {
         m_playerInputActions.PlayerMovement.Disable();
@@ -34,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         m_rb = gameObject.GetComponent<Rigidbody>();
+        m_currentMoveSpeed = maxMoveSpeed;
     }
     
     private void Update()
@@ -52,13 +60,28 @@ public class PlayerMovement : MonoBehaviour
         
         if (m_input.magnitude >= 0.1f)
         {
-            m_rb.velocity = moveSpeed * Time.fixedDeltaTime * m_input.y * forward + moveSpeed * Time.fixedDeltaTime * m_input.x * right + m_rb.velocity.y * Vector3.up;
+            m_rb.velocity = m_currentMoveSpeed * Time.fixedDeltaTime * m_input.y * forward + m_currentMoveSpeed * Time.fixedDeltaTime * m_input.x * right + m_rb.velocity.y * Vector3.up;
         }
     }
     
     private void JumpPerformed(InputAction.CallbackContext context)
     {
         m_rb.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+    }
+    
+    private void Walk(InputAction.CallbackContext context)
+    {
+        Debug.Log("WALK");
+        if (context.performed)
+        {
+            Debug.Log("Shift pressed");
+            m_currentMoveSpeed /= 2;
+        }
+        else if (context.canceled)
+        {
+            Debug.Log("Shift released");
+            m_currentMoveSpeed = maxMoveSpeed;
+        }
     }
 }
 
