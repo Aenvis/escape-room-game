d4a4d5dbf6257c37913551fc688ee1c14bdb9c78
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2022-10-16 11:39:03 +0200

    Adding Menu (PlayButton, Settings, Credis, Quit)

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+   {
+      SceneManager.LoadScene("devEventSystem");
+   }
+
+   public void GoToSettingMenu()
+   {
+      SceneManager.LoadScene("SettingMenu");
+   }
+
+   public void GoToMainMenu()
+   {
+      SceneManager.LoadScene("MainMenu");
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+   }
+
+}
+
