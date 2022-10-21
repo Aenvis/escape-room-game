0712d5b68958d39f6313a66c3cfb3845db5f753e
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-10-21 23:11:20 +0200

    more GameEvent types

---
+++ Assets/_Scripts/_Systems/GameEvents/GameEventListenerStr.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Project.Systems.GameEvents
+{
+    [System.Serializable]
+    public class UnityEventStr : UnityEvent<string>
+    {
+    }
+    public class GameEventListenerStr : GameEventListener
+    {
+        [SerializeField] [CanBeNull] private UnityEventStr response;
+
+        public void OnEventRaised(string str)=> response?.Invoke(str);
+        
+    }
+}
