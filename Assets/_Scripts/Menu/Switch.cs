f4abb153d81f775800307c758f30c580e6410ebd
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2022-10-31 14:22:37 +0100

    Added change UI image in Menu

---
+++ Assets/_Scripts/Menu/Switch.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+    public class Switch : MonoBehaviour
+    {
+        public GameObject[] background;
+        private int index;
+        private float update;
+        private void Start()
+        {
+            update = 0.0f;
+            index = 0;
+        }
+
+        private void Update()
+        {
+            update += Time.deltaTime;
+            for (int i = 0; update > 5.0f;)
+                {
+                    index += 1;
+                    update = 0.0f;
+                    background[i].gameObject.SetActive(false);
+                    background[index].gameObject.SetActive(true);
+
+                    if (i >= 5)
+                        index = 0;
+                    if (i < 0)
+                        index = 5;
+
+                    UnityEngine.Debug.Log("Update");
+                }
+            
+            
+        }
+    }
+}
