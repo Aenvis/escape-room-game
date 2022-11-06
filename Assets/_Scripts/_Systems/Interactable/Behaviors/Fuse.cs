f1a384f8b632ff1e03ad662e272ef29b494c5f91
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-11-06 20:42:27 +0100

    added fuse script

---
+++ Assets/_Scripts/_Systems/Interactable/Behaviors/Fuse.cs
@@ -0,0 +1,13 @@
+﻿using Project.Consts;
+using UnityEngine;
+
+namespace Project.Systems.Interactable
+{
+    public class Fuse : Usable
+    {
+        protected override void SetName()
+        {
+            Name = ItemName.fuse;
+        }
+    }
+}
