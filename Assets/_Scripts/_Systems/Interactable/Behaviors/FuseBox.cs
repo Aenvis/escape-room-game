86db86ea3ba4e65f917c6855a85b655182b7e44d
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-11-19 23:54:44 +0100

    Add repairing machines and displays

--- Assets/_Scripts/_Systems/Interactable/Behaviors/FuseBox.cs
+++ Assets/_Scripts/_Systems/Interactable/Behaviors/FuseBox.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using Project.Systems.GameEvents;
+using UnityEngine;
 
 namespace Project.Systems.Interactable
 {
     public class FuseBox : Interactable
     {
         [SerializeField] private GameObject fuse;
+        [SerializeField] private GameEvent gameEvent;
+        
         
         protected override void Interaction()
         {
             if (fuse.activeSelf) return;
+            gameEvent.Invoke();
             fuse.SetActive(true);
         }
     }
